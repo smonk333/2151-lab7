ed970db6e107da0a7366206dce98f986244366cb
garrett <69862547+smonk333@users.noreply.github.com>
2023-11-07 12:18:03 -0500

    fixed spacing

--- src/cpsc2150/listDec/IShuffleList.java
+++ src/cpsc2150/listDec/IShuffleList.java
@@ -3,6 +3,8 @@ package cpsc2150.listDec;
 import java.util.List;
 
 public interface IShuffleList<T> extends List<T> {
+
+
     default void shuffle(int swaps) {
         for (int i = 0; i < swaps; i++) {
             int index1 = (int) (Math.random() * size());
